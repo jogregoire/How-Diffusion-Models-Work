9e540d82099221d8a4e8170bf27b8a795d86d6ae
Joël Gregoire <joel.gregoire@ubisoft.com>
2024-03-08 14:38:43 +0100

    prettified memory tracking

--- joel/gpuperf.py
+++ joel/gpuperf.py
@@ -7,17 +7,19 @@ class GPUPerf:
         self.device = device
         device_name = torch.cuda.get_device_name(0)
         device_count = torch.cuda.device_count()
-        self.mem_total = torch.cuda.get_device_properties(0).total_memory/(1024**2)
-        log.info(f"Device: {device_name}, Count: {device_count}, Memory: {self.mem_total/1024:.2f} GB")
+        self.mem_total = torch.cuda.get_device_properties(0).total_memory
+        log.info(f"Device: {device_name}, Count: {device_count}, Memory: {self.__bytefmt(self.mem_total)} GB")
     
     def snapshot(self, event="None"):
         mem = torch.cuda.memory_stats(device=self.device)
-        allocated = mem['active_bytes.all.allocated']/(1024**2)
-        freed = mem['active_bytes.all.freed']/(1024**2)
-        current = mem['active_bytes.all.current']/(1024**2) # = allocated - freed
-        peak = mem['active_bytes.all.peak']/(1024**2)
+        allocated = mem['active_bytes.all.allocated']
+        freed = mem['active_bytes.all.freed']
+        current = mem['active_bytes.all.current'] # = allocated - freed
+        peak = mem['active_bytes.all.peak']
         available_at_peak = self.mem_total - peak
-        log.info(f"Event: {event} Allocated: {allocated:.2f} MB, Freed: {freed:.2f} MB, Current: {current:.2f} MB, Available at Peak: {available_at_peak:.2f} MB")
+
+        # Now, use this function in your log message
+        log.info(f"Event: {event} Current: {self.__bytefmt(current)}, Peak: {self.__bytefmt(peak)}, Available at Peak: {self.__bytefmt(available_at_peak)}, Allocated total: {self.__bytefmt(allocated)}, Freed: {self.__bytefmt(freed)}")
 
     def record_memory_history(self):
         # record memory usage: https://pytorch.org/docs/stable/torch_cuda_memory.html
@@ -26,3 +28,15 @@ class GPUPerf:
         
     def dump_memory_history(self):
         torch.cuda.memory._dump_snapshot("my_snapshot.pickle")
+
+    def __bytefmt(self, byte_size):
+        # Define the suffixes for each size
+        suffixes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB']
+
+        # Find the index of the suffix we'll use
+        i = 0
+        while byte_size >= 1024 and i < len(suffixes)-1:
+            byte_size /= 1024.
+            i += 1
+        f = ('%.2f' % byte_size).rstrip('0').rstrip('.')
+        return f'{f} {suffixes[i]}'
